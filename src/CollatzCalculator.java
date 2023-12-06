e15bf7b7745035530490299eec2d93fa5eb4bfbd
David Tucker <david.tucker@email.com>
2023-12-06 01:17:09 +0000

    Initial recursive implementation. Beware stack overflow for high numbers. In Scala ise @tailrec annotation to avoid.

---
+++ src/CollatzCalculator.java
@@ -0,0 +1,13 @@
+/**
+ * Class to calculate numbers around Collatz conjecture
+ * <a href="https://en.wikipedia.org/wiki/Collatz_conjecture">Collatz Conjecture on Wikipedia</a>
+ */
+public class CollatzCalculator {
+    public static long calculateNextNumber(final long priorNumber) {
+        return priorNumber % 2 == 0 ? priorNumber / 2 : (priorNumber * 3) + 1;
+    }
+
+    public static long calculateCollatzSteps(final long number) {
+        return number == 1 ? 0 : 1 + calculateCollatzSteps(calculateNextNumber(number));
+    }
+}
---
+++ src/CollatzCalculatorTest.java
@@ -0,0 +1,21 @@
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class CollatzCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateNextNumberTest() {
+        assertThat(CollatzCalculator.calculateNextNumber(2L), equalTo(1L));
+        assertThat(CollatzCalculator.calculateNextNumber(3L), equalTo(10L));
+        assertThat(CollatzCalculator.calculateNextNumber(4L), equalTo(2L));
+        assertThat(CollatzCalculator.calculateNextNumber(5L), equalTo(16L));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCollatzStepCountForNumberTest() {
+        assertThat(CollatzCalculator.calculateCollatzSteps(1L), equalTo(0L));
+        assertThat(CollatzCalculator.calculateCollatzSteps(2L), equalTo(1L));
+        assertThat(CollatzCalculator.calculateCollatzSteps(4L), equalTo(2L));
+        assertThat(CollatzCalculator.calculateCollatzSteps(5L), equalTo(5L));
+    }
+}
